d38e37cc0eee1ddbaff0f8865ba40a6b0037acc3
Zoltan Bonus <Zoltan.Bonus@dealogic.com>
2020-04-10 14:05:48 +0200

    Pythonize tests

--- projects/2020/0/degrees/tests.py
+++ projects/2020/0/degrees/tests.py
@@ -1,33 +1,36 @@
+import unittest
 import csv
-import sys
-
 from degrees import load_data, shortest_path, person_id_for_name
 from util import Node, StackFrontier, QueueFrontier
 
 
-def main():
-    load_data("small")
-    try:
-        test_shortest_path("Kevin Bacon", "Kevin Bacon", 1)
-        test_shortest_path("Kevin Bacon", "Tom Cruise", 1)
-        test_shortest_path("Kevin Bacon", "Cary Elwes", 3)
-    except Exception as e:
-        print("Test failed. " + str(e))
+class TestShortestPath(unittest.TestCase):
+    def test_find_further_node(self):
+        self.assertEqual(len(self.get_shortest_path(
+            "small", "Kevin Bacon", "Cary Elwes")), 3)
+
+    def test_find_same_actor(self):
+        self.assertEqual(len(self.get_shortest_path(
+            "small", "Kevin Bacon", "Kevin Bacon")), 1)
+
+    def test_find_neighbor_node(self):
+        self.assertEqual(len(self.get_shortest_path(
+            "small", "Kevin Bacon", "Tom Cruise")), 1)
+
+    def test_no_path(self):
+        self.assertEqual(self.get_shortest_path(
+            "small", "Kevin Bacon", "Emma Watson"), None)
+
+    def test_large_data(self):
+        self.assertEqual(len(self.get_shortest_path(
+            "large", "Emma Watson", "Jennifer Lawrence")), 3)
+
+    def get_shortest_path(self, directory, sourceName, targetName):
+        load_data(directory)
+        source = person_id_for_name(sourceName)
+        target = person_id_for_name(targetName)
+        return shortest_path(source, target)
 
 
-def test_shortest_path(sourceName, targetName, expectedPathLength):
-    print(
-        f"Executing test: sourceName: {sourceName}, targetName: {targetName}, expectedPathLength: {expectedPathLength}")
-    source = person_id_for_name(sourceName)
-    target = person_id_for_name(targetName)
-
-    path = shortest_path(source, target)
-    if len(path) != expectedPathLength:
-        raise Exception(
-            f"Invalid path length. Expected {expectedPathLength}, Actual: {len(path)}")
-    else:
-        print("Test passed!")
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    unittest.main()
