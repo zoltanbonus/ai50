74140f43e255989e7b644e1cfe4a20da33bdc4aa
Zoltan Bonus <Zoltan.Bonus@dealogic.com>
2020-04-12 10:54:15 +0200

    Initial files for tictactoe

---
+++ projects/2020/x/tictactoe/tests.py
@@ -0,0 +1,158 @@
+import unittest
+from tictactoe import player, X, EMPTY, O, initial_state, actions, result, winner
+
+
+class TestShortestPath(unittest.TestCase):
+    def test_player_initial_state(self):
+        self.assertEqual(player(initial_state()), X)
+
+    def test_player_Os_move(self):
+        board = [[X, EMPTY, EMPTY],
+                 [EMPTY, EMPTY, EMPTY],
+                 [EMPTY, EMPTY, EMPTY]]
+        self.assertEqual(player(board), O)
+
+    def test_player_Xs_move(self):
+        board = [[X, EMPTY, EMPTY],
+                 [EMPTY, O, EMPTY],
+                 [EMPTY, EMPTY, EMPTY]]
+        self.assertEqual(player(board), X)
+
+    def test_actions_initial_state(self):
+        expected = {(0, 0), (0, 1), (0, 2),
+                    (1, 0), (1, 1), (1, 2),
+                    (2, 0), (2, 1), (2, 2)}
+
+        self.assertEqual(actions(initial_state()), expected)
+
+    def test_actions_mid_game(self):
+        board = [[X, EMPTY, EMPTY],
+                 [EMPTY, O, EMPTY],
+                 [X, EMPTY, EMPTY]]
+
+        expected = {(0, 1), (0, 2),
+                    (1, 0), (1, 2),
+                    (2, 1), (2, 2)}
+
+        self.assertEqual(actions(board), expected)
+
+    def test_actions_end_game(self):
+        board = [[X, X, O],
+                 [O, O, EMPTY],
+                 [X, O, X]]
+
+        expected = {(1, 2)}
+
+        self.assertEqual(actions(board), expected)
+
+    def test_actions_terminal_board(self):
+        board = [[X, X, O],
+                 [O, O, O],
+                 [X, O, X]]
+
+        expected = set()
+
+        self.assertEqual(actions(board), expected)
+
+    def test_result_input_board_unmodified(self):
+        board = initial_state()
+        action = (0, 0)
+
+        result(board, action)
+        self.assertEqual(board, initial_state())
+
+    def test_result_initial_state(self):
+        board = initial_state()
+        action = (0, 0)
+        expected = [[X, EMPTY, EMPTY],
+                    [EMPTY, EMPTY, EMPTY],
+                    [EMPTY, EMPTY, EMPTY]]
+
+        self.assertEqual(result(board, action), expected)
+
+    def test_result_mid_game(self):
+        board = [[X, EMPTY, X],
+                 [O, O, EMPTY],
+                 [X, EMPTY, EMPTY]]
+        action = (1, 2)
+        expected = [[X, EMPTY, X],
+                    [O, O, O],
+                    [X, EMPTY, EMPTY]]
+
+        self.assertEqual(result(board, action), expected)
+
+    def test_result_invalid_move(self):
+        board = [[X, EMPTY, X],
+                 [O, O, EMPTY],
+                 [X, EMPTY, EMPTY]]
+        action = (0, 0)
+
+        self.assertRaises(Exception, result, board, action)
+
+    def test_winner_initial_state(self):
+        board = initial_state()
+        self.assertEqual(winner(board), None)
+
+    def test_winner_mid_game(self):
+        board = [[X, EMPTY, X],
+                 [O, O, EMPTY],
+                 [X, EMPTY, EMPTY]]
+        self.assertEqual(winner(board), None)
+
+    def test_winner_tie(self):
+        board = [[X, X, O],
+                 [O, O, X],
+                 [X, O, X]]
+        self.assertEqual(winner(board), None)
+
+    def test_winner_vertical_0(self):
+        board = [[X, EMPTY, O],
+                 [X, EMPTY, O],
+                 [X, EMPTY, X]]
+        self.assertEqual(winner(board), X)
+
+    def test_winner_vertical_1(self):
+        board = [[O, X, O],
+                 [X, X, O],
+                 [O, X, X]]
+        self.assertEqual(winner(board), X)
+
+    def test_winner_vertical_2(self):
+        board = [[X, EMPTY, O],
+                 [X, EMPTY, O],
+                 [EMPTY, EMPTY, O]]
+        self.assertEqual(winner(board), O)
+
+    def test_winner_horizontal_0(self):
+        board = [[X, X, X],
+                 [O, EMPTY, O],
+                 [EMPTY, EMPTY, O]]
+        self.assertEqual(winner(board), X)
+
+    def test_winner_horizontal_1(self):
+        board = [[X, O, X],
+                 [O, O, O],
+                 [EMPTY, X, X]]
+        self.assertEqual(winner(board), X)
+
+    def test_winner_horizontal_2(self):
+        board = [[X, O, X],
+                 [O, O, EMPTY],
+                 [X, X, X]]
+        self.assertEqual(winner(board), X)
+
+    def test_winner_diagonal_0(self):
+        board = [[X, O, O],
+                 [O, X, EMPTY],
+                 [O, X, X]]
+        self.assertEqual(winner(board), X)
+
+    def test_winner_diagonal_1(self):
+        board = [[O, O, X],
+                 [O, X, EMPTY],
+                 [X, X, O]]
+        self.assertEqual(winner(board), X)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ projects/2020/x/tictactoe/tictactoe.py
@@ -0,0 +1,94 @@
+"""
+Tic Tac Toe Player
+"""
+
+import math
+import numpy as np
+import copy
+
+X = "X"
+O = "O"
+EMPTY = None
+
+
+def initial_state():
+    """
+    Returns starting state of the board.
+    """
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
+
+
+def player(board):
+    """
+    Returns player who has the next turn on a board.
+    """
+    if board == initial_state():
+        return X
+
+    numpyBoard = np.array(board)
+    xCount = np.count_nonzero(numpyBoard == X)
+    oCount = np.count_nonzero(numpyBoard == O)
+
+    if xCount > oCount:
+        return O
+    else:
+        return X
+
+
+def actions(board):
+    """
+    Returns set of all possible actions (i, j) available on the board.
+    """
+    result = set()
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == EMPTY:
+                result.add((i, j))
+
+    return result
+
+
+def result(board, action):
+    """
+    Returns the board that results from making move (i, j) on the board.
+    """
+    i = action[0]
+    j = action[1]
+
+    if board[i][j] != EMPTY:
+        raise Exception('Invalid action')
+
+    nextPlayer = player(board)
+    newBoard = copy.deepcopy(board)
+    newBoard[i][j] = nextPlayer
+    return newBoard
+
+
+def winner(board):
+    """
+    Returns the winner of the game, if there is one.
+    """
+    raise NotImplementedError
+
+
+def terminal(board):
+    """
+    Returns True if game is over, False otherwise.
+    """
+    raise NotImplementedError
+
+
+def utility(board):
+    """
+    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
+    """
+    raise NotImplementedError
+
+
+def minimax(board):
+    """
+    Returns the optimal action for the current player on the board.
+    """
+    raise NotImplementedError
